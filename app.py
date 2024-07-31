c10261040e9726f60c19c36c920150a680f7a9ea
Pro <Pro@MacBook-Air-de-gregoire.local>
2024-07-31 18:41:31 +0200

    Initial commit with complete Flask app

--- app.py
+++ app.py
@@ -1,25 +1,36 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, request, redirect, render_template, session
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
+app.secret_key = 'your_secret_key'  # Remplacez 'your_secret_key' par une clé secrète sécurisée
 
 @app.route('/')
 def home():
-    return redirect(url_for('create_routine'))
+    return redirect('/create')
 
 @app.route('/create', methods=['GET', 'POST'])
-def create_routine():
+def create():
     if request.method == 'POST':
-        exercises = request.form.getlist('exercise')
-        durations = request.form.getlist('duration')
-        session['routine'] = list(zip(exercises, durations))
-        return redirect(url_for('start_training'))
-    return render_template('create.html')
+        exercise = request.form.get('exercise')
+        duration = request.form.get('duration')
+        if 'routine' not in session:
+            session['routine'] = []
+        # Ajouter l'exercice à la session
+        session['routine'].append((exercise, duration))
+        session.modified = True  # Marquer la session comme modifiée
+        return redirect('/create')
+    return render_template('create.html', routine=session.get('routine', []))
 
-@app.route('/train', methods=['GET'])
-def start_training():
+@app.route('/train')
+def train():
     routine = session.get('routine', [])
     return render_template('train.html', routine=routine)
+
+@app.route('/remove/<int:index>', methods=['POST'])
+def remove(index):
+    if 'routine' in session and 0 <= index < len(session['routine']):
+        del session['routine'][index]
+        session.modified = True  # Marquer la session comme modifiée
+    return redirect('/create')
 
 if __name__ == '__main__':
     app.run(debug=True)
