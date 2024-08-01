67c7e02245b817d0ebd6ec7ca958c77b12918deb
Pro <Pro@MacBook-Air-de-gregoire.local>
2024-08-01 14:05:38 +0200

    update for WPA

---
+++ static/service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'training-timer-cache-v1';
+const urlsToCache = [
+  '/',
+  '/static/manifest.json',
+  '/static/icons/icon-192x192.png',
+  '/static/icons/icon-512x512.png',
+  '/static/styles.css',  // Ajoutez d'autres fichiers CSS/JS si nécessaire
+  '/static/manifest.json'
+];
+
+// Installation du service worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Gestion des requêtes
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Mise à jour du service worker
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
